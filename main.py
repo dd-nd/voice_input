6eb842a9a872d49f6c036971add1ae6d35f0e61a
dd-nd <lya.yop@yandex.ru>
2024-02-12 15:10:07 +0300

    база

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print('ff')
+import speech_recognition as sr
 
+recognizer = sr.Recognizer()
 
-if __name__ == "__main__":
-    main()
+with sr.Microphone() as source:
+    print("говорите")
+    recognizer.adjust_for_ambient_noise(source)
+    audio_data = recognizer.listen(source)
+
+    print("молчите для остановки записи")
+    try:
+        text = recognizer.recognize_google(audio_data, language="ru-RU")
+        print("сказано: " + text)
+    except sr.UnknownValueError:
+        print("ошибка: Голос не распознан")
+    except sr.RequestError as e:
+        print(f"ошибка сервиса распознавания речи; {e}")
