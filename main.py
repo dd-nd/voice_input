d05e47f386fd4540656c77f7356185fd222ae0e3
dd-nd <lya.yop@yandex.ru>
2024-02-13 10:04:52 +0300

    upd

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import speech_recognition as sr
 import pyaudio
 import wave
-import params as par
+
+CHUNK = 1024    # определяет форму ауди сигнала
+FRT = pyaudio.paInt16   # 16-битный формат -- значение амплитуды
+CHAN = 1        # канал записи звука
+RT = 44100      # частота 
+REC_SEC = 5     #длина записи
+OUTPUT = 'out.wav'
 
 p = pyaudio.PyAudio()
 
 # запись аудио
-with p.open(format=par.FRT, channels=par.CHAN, rate=par.RT, 
-            input=True, frames_per_buffer=par.CHUNK) as stream:
-    print('идет запись')
-    frames = []
+stream = p.open(format=FRT, channels=CHAN, rate=RT, input=True, frames_per_buffer=CHUNK)
+print('идет запись')
+frames = []
 
-    for i in range(0, int(par.RT / par.CHUNK * par.REC_SEC)):
-        data = stream.read(par.CHUNK)
-        frames.append(data)
+for i in range(0, int(RT / CHUNK * REC_SEC)):
+    data = stream.read(CHUNK)
+    frames.append(data)
 
-    print('запись выполнена')
-    stream.stop_stream()
-# stream.close()
+print('запись выполнена')
+stream.stop_stream()
+
+stream.close()
 p.terminate()
 
 # запись в файл
-with wave.open(par.OUTPUT, 'wb') as w:
-    w.setnchannels(par.CHAN)
-    w.setsampwidth(p.get_sample_size(par.FRT))
-    w.setframerate(par.RT)
+with wave.open(OUTPUT, 'wb') as w:
+    w.setnchannels(CHAN)
+    w.setsampwidth(p.get_sample_size(FRT))
+    w.setframerate(RT)
     w.writeframes(b''.join(frames))
 # w.close()
 
 # открытие файда
-record_file = sr.WavFile(par.OUTPUT)
+record_file = sr.WavFile(OUTPUT)
 r = sr.Recognizer()
 
 with record_file as audio:
