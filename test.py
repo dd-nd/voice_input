185f73e30baf63dc86ace1eba8eace93e852802b
dd-nd <lya.yop@yandex.ru>
2024-02-15 10:30:45 +0300

    add

--- test.py
+++ test.py
@@ -1,21 +1,27 @@
+# import whisper
+
+# model = whisper.load_model("base")
+
+# # load audio and pad/trim it to fit 30 seconds
+# audio = whisper.load_audio("out.mp3")
+# audio = whisper.pad_or_trim(audio)
+
+# # make log-Mel spectrogram and move to the same device as the model
+# mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# # detect the spoken language
+# _, probs = model.detect_language(mel)
+# print(f"Detected language: {max(probs, key=probs.get)}")
+
+# # decode the audio
+# options = whisper.DecodingOptions()
+# result = whisper.decode(model, mel, options)
+
+# # print the recognized text
+# print(result.text)
+
 import whisper
 
 model = whisper.load_model("base")
-
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("out.mp3")
-audio = whisper.pad_or_trim(audio)
-
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
-
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
-
-# print the recognized text
-print(result.text)
+result = model.transcribe("out.mp3")
+print(result["text"])
