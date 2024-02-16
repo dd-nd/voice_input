afd29ccf442a804d362b7909ec2b61d7aa8d19f0
dd-nd <lya.yop@yandex.ru>
2024-02-16 14:25:16 +0300

    инкрустирован openai-whisper

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import sys
 import threading
 from transformers import pipeline
 
+
 CHUNK = 1024    # определяет форму аудиосигнала
 FRT = pyaudio.paInt32   # значение амплитуды
 RT = 44100      # частота 
-# REC_SEC = 5     #длина записи
-OUTPUT = 'out.flac'
+OUTPUT = 'out.wav'
 STOP_SYMBOL = ''
 
 p = pyaudio.PyAudio()
+
 
 def save_frames(frame):    # СОЗДАНИЕ .WAV ФАЙЛА (1)
     with wave.open(OUTPUT, 'wb') as w:
@@ -41,22 +42,11 @@ def record_audio():     # ЗАПИСЬ АУДИО (2)
 
 
 def open_wav():     # ОТКРЫТИЕ ФАЙЛА (3)
-    record_file = sr.WavFile(OUTPUT)
-    # r = sr.Recognizer()
-
-    with record_file as audio:
-        try:
-            # r.adjust_for_ambient_noise(audio)
-            # print(r.recognize_google(r.record(audio), language='ru-RU'))
-
-            transcriber = pipeline(model="openai/whisper-medium", generate_kwargs={"language": "russian"})
-            transcriber(OUTPUT)
-
-        except sr.UnknownValueError:
-            print('Не удалось распознать речь')
-        except sr.RequestError as e:
-            print(f'Ошибка при обращении к сервису распознавания речи; {e}')
-
+    try:
+        transcriber = pipeline(model="openai/whisper-medium", generate_kwargs={"language": "russian"})
+        print(transcriber(OUTPUT)['text'])  # json {'text':'...value...'}
+    except Exception as e:
+        print(e)
 
 def stop_input():
     global STOP_SYMBOL
@@ -73,44 +63,3 @@ if __name__ == '__main__':
     stop_thread.start()
 
     audio_thread.join()
-
-
-
-
-# ОРИГИНАЛ (РАБОТАЕТ)
-# # запись аудио
-# stream = p.open(format=FRT, channels=1, rate=RT, input=True, frames_per_buffer=CHUNK)
-# print('идет запись')
-# frames = []
-
-# for i in range(0, int(RT / CHUNK * REC_SEC)):
-#     data = stream.read(CHUNK)
-#     frames.append(data)
-
-# print('запись выполнена')
-# stream.stop_stream()
-
-# stream.close()
-# p.terminate()
-        
-
-
-# # воспроизведение записи
-# with wave.open(OUTPUT, 'rb') as wf:
-#     def callback(in_data, frame_count, time_info, status):
-#         data = wf.readframes(frame_count)
-#         return (data, pyaudio.paContinue)
-
-#     p = pyaudio.PyAudio()
-
-#     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-#                     channels=wf.getnchannels(),
-#                     rate=wf.getframerate(),
-#                     output=True,
-#                     stream_callback=callback)
-
-#     while stream.is_active():
-#         time.sleep(0.1)
-
-#     stream.close()
-#     p.terminate()
