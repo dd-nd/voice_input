759fa176ccd86aeaadb608d3ddd4036881aadef1
dd-nd <lya.yop@yandex.ru>
2024-02-16 15:37:12 +0300

    optim

--- main.py
+++ main.py
@@ -1,58 +1,61 @@
-import speech_recognition as sr
-import pyaudio
-import wave
-import sys
 import threading
+import wave
+import pyaudio
 from transformers import pipeline
 
-
-CHUNK = 1024    # определяет форму аудиосигнала
-FRT = pyaudio.paInt32   # значение амплитуды
-RT = 44100      # частота 
+CHUNK = 1024                    # форма аудиосигнала
+FRT = pyaudio.paInt32           # значение амплитуды
+RT = 44100                      # частота 
 OUTPUT = 'out.wav'
-STOP_SYMBOL = ''
 
+STOP_SYMBOL = threading.Event()
 p = pyaudio.PyAudio()
 
-
-def save_frames(frame):    # СОЗДАНИЕ .WAV ФАЙЛА (1)
+def save_frames(frames):        # СОЗДАНИЕ .WAV ФАЙЛА (1)
     with wave.open(OUTPUT, 'wb') as w:
         w.setnchannels(1)
         w.setsampwidth(p.get_sample_size(FRT))
         w.setframerate(RT)
-        w.writeframes(b''.join(frame))
+        w.writeframes(b''.join(frames))
 
 
-def record_audio():     # ЗАПИСЬ АУДИО (2)
+def record_audio():             # ЗАПИСЬ АУДИО (2)
     stream = p.open(format=FRT, channels=1, rate=RT, input=True, frames_per_buffer=CHUNK)
-    print('идет запись...')
     frames = []
 
-    while STOP_SYMBOL != '/':
+    print('Записываем...')
+    while not STOP_SYMBOL.is_set():
         data = stream.read(CHUNK)
         frames.append(data)
 
-    print('запись выполнена!')
+    # while STOP_SYMBOL != '/':
+    #     data = stream.read(CHUNK)
+    #     frames.append(data)
+
+    print('Запись сделана!')
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    save_frames(frame=frames)   # (1)
-    open_wav()  # (3)
+    save_frames(frames)         # (1)
+    open_wav()                  # (3)
 
 
-def open_wav():     # ОТКРЫТИЕ ФАЙЛА (3)
+def open_wav():                 # ОТКРЫТИЕ ФАЙЛА (3)
     try:
-        transcriber = pipeline(model="openai/whisper-medium", generate_kwargs={"language": "russian"})
-        print(transcriber(OUTPUT)['text'])  # json {'text':'...value...'}
+        transcriber = pipeline(model="openai/whisper-large", generate_kwargs={"language": "russian"})
+        print(transcriber(OUTPUT)['text'])  # json
     except Exception as e:
         print(e)
 
-def stop_input():
-    global STOP_SYMBOL
 
-    print('для остановки записи введите /')
-    STOP_SYMBOL = sys.stdin.read(1)
+def stop_input():
+    input('Для завершения записи нажмине Enter\n')
+    STOP_SYMBOL.set()
+    
+    # global STOP_SYMBOL
+    # print('для остановки записи введите /')
+    # STOP_SYMBOL = sys.stdin.read(1)
 
 
 if __name__ == '__main__':
