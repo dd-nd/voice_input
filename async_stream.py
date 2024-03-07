c36c30c2868bbdcf8f0f56f2177bcc2002663710
dd-nd <lya.yop@yandex.ru>
2024-03-07 14:51:59 +0300

    fl

--- async_stream.py
+++ async_stream.py
@@ -1,8 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 import asyncio
 import numpy as np
 import pyaudio
 from transformers import pipeline
 import keyboard
+from waitress import serve
 import librosa
 
 CHUNK = 1024                    # форма аудиосигнала
@@ -10,12 +13,16 @@ FRT = pyaudio.paFloat32         # значение амплитуды
 ORIGINAL_RATE = 44100           # исходная частота дискретизации
 NEW_RATE = 16000                # новая частота дискретизации
 
+app = Flask(__name__)
+CORS(app)
+
 p = pyaudio.PyAudio()
 
 # Функция для преобразования аудио в текст
-async def transcribe_audio():
+@app.route('/transcribe', methods=['POST'])
+def transcribe_audio():
     try:
-        transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-small", generate_kwargs={"language": "russian"})
+        transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3", generate_kwargs={"language": "russian"})
         stream = p.open(format=FRT, channels=1, rate=ORIGINAL_RATE, input=True, frames_per_buffer=CHUNK)
 
         print("Слушаем...")
@@ -39,13 +46,15 @@ async def transcribe_audio():
         # Модель
         transcription = transcriber(librosa.resample(all_data, orig_sr=ORIGINAL_RATE, target_sr=NEW_RATE)).get("text")
         if transcription:
-            print("Текст:", transcription)
-
+            response = jsonify({'transcription': transcription})
+            response.headers['Access-Control-Allow-Origin'] = '*'  # Устанавливаем заголовок для CORS
+            return response
     except Exception as e:
-        print(e)
+        return jsonify({'error': str(e)})
 
-async def main():                   
-    await asyncio.gather(transcribe_audio())
+# async def main():                   
+#     await asyncio.gather(transcribe_audio())
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8080)
